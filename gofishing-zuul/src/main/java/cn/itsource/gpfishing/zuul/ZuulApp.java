a2f3b7bbb25aa71664bf253eb9c1a32e873fa7aa
one <one@163.com>
2019-10-11 18:29:18 +0800

    解决无法拉取的bug

--- gofishing-zuul/src/main/java/cn/itsource/gpfishing/zuul/ZuulApp.java
+++ gofishing-zuul/src/main/java/cn/itsource/gpfishing/zuul/ZuulApp.java
@@ -2,10 +2,12 @@ package cn.itsource.gpfishing.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulApp {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApp.class,args);
